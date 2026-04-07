f0dfcdb971017ed87889847e667669ede3f09175
lautaeferdinandus831021-lgtm <lautaeferdinandus831021@gmail.com>
2026-04-07 14:58:22 +0000

    fix ws market stable

--- ws/market.js
+++ ws/market.js
@@ -24,7 +24,6 @@ function connectMarket(state){
 
     if(data.data){
       const ticker = data.data[0];
-
       state.price = parseFloat(ticker.last);
       console.log('PRICE:', state.price);
     }
@@ -34,46 +33,7 @@ function connectMarket(state){
     console.log('MARKET RECONNECT...');
     setTimeout(()=>connectMarket(state),2000);
   });
+
 }
 
-module.exports = {connectMarket};
-EOFcat <<EOF > ws/market.js
-const WebSocket = require('ws');
-
-let ws;
-
-function connectMarket(state){
-
-  ws = new WebSocket('wss://ws.bitget.com/v2/ws/public');
-
-  ws.on('open', ()=>{
-    console.log('MARKET WS OPEN');
-
-    ws.send(JSON.stringify({
-      op: "subscribe",
-      args: [{
-        instType: "UMCBL",
-        channel: "ticker",
-        instId: "BTCUSDT"
-      }]
-    }));
-  });
-
-  ws.on('message', (msg)=>{
-    const data = JSON.parse(msg.toString());
-
-    if(data.data){
-      const ticker = data.data[0];
-
-      state.price = parseFloat(ticker.last);
-      console.log('PRICE:', state.price);
-    }
-  });
-
-  ws.on('close', ()=>{
-    console.log('MARKET RECONNECT...');
-    setTimeout(()=>connectMarket(state),2000);
-  });
-}
-
-module.exports = {connectMarket};
+module.exports = { connectMarket };
