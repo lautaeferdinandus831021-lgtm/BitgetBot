27e5969be2859ef092e71d929661d35942896c37
lautaeferdinandus831021-lgtm <lautaeferdinandus831021@gmail.com>
2026-04-11 14:44:13 +0000

    update bot logic

---
+++ core/debug.js
@@ -0,0 +1,5 @@
+require('dotenv').config();
+
+console.log('API_KEY:', process.env.API_KEY);
+console.log('API_SECRET:', process.env.API_SECRET);
+console.log('PASSPHRASE:', process.env.PASSPHRASE);
