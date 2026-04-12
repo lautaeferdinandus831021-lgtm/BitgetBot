f394f2c2945692750629d5128ffa3b47af0240ea
lautaeferdinandus831021-lgtm <lautaeferdinandus831021@gmail.com>
2026-04-12 03:37:50 +0000

    update bot logic

--- market/data.js
+++ market/data.js
@@ -1,3 +1,18 @@
-module.exports = {
-    getPrice: () => Math.random() * 100000
-};
+const axios = require('axios');
+
+async function getPrice() {
+    try {
+        const res = await axios.get(
+            'https://api.bitget.com/api/v2/spot/market/ticker?symbol=BTCUSDT'
+        );
+
+        const price = parseFloat(res.data.data[0].lastPr);
+        return price;
+
+    } catch (err) {
+        console.log('❌ PRICE ERROR:', err.message);
+        return null;
+    }
+}
+
+module.exports = { getPrice };
